721ec78a8a81f17190ed6c140bbb7bfeb50e4edb
rajiii21 <prajeshwari313@gmail.com>
2023-10-06 15:58:26 +0530

    addnew

---
+++ main.js
@@ -0,0 +1,8 @@
+// let length=parseInt(prompt("enter your length"));
+// let width=parseInt(prompt("enter your width"));
+
+
+let regtangular=function(length,width){
+	return length*width;
+}
+document.write(regtangular(3,4));
